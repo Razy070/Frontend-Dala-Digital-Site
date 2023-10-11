8412aeb7c4582ddb516370a2bc87c53d56b2427d
RaZy <105773114+Razy070@users.noreply.github.com>
2023-10-11 17:28:11 +0700

    UP

--- mobile-version-reade-page-5/scripts/menu.js
+++ mobile-version-reade-page-5/scripts/menu.js
@@ -2,12 +2,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.navbar-flex');
     const menu = document.getElementById('menu');
+    const langSwitch = document.getElementById('lang-switch')
+
 
     button.addEventListener('click', () => {
         if (menu.style.display === 'flex') {
             menu.style.display = 'none';
+            langSwitch.style.display = 'flex'
+
         } else {
             menu.style.display = 'flex';
+            langSwitch.style.display = 'none'
         }
     });
 
--- mobile-version-reade-page-6/scripts/menu.js
+++ mobile-version-reade-page-6/scripts/menu.js
@@ -2,12 +2,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.navbar-flex');
     const menu = document.getElementById('menu');
+    const langSwitch = document.getElementById('lang-switch')
+    const toptext = document.getElementById('toptext')
 
     button.addEventListener('click', () => {
         if (menu.style.display === 'flex') {
             menu.style.display = 'none';
+            langSwitch.style.display = 'flex'
+            toptext.style.display = 'block'
         } else {
             menu.style.display = 'flex';
+            langSwitch.style.display = 'none'
+            toptext.style.display = 'none'
         }
     });
 
